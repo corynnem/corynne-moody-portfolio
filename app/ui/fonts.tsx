9300af96cb6ef3cf6e866d46270a4b22589cbc84
Corynne Moody <corynnemoody@MacBookPro.attlocal.net>
2025-08-11 18:42:09 -0400

    resolve vercel issue

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const mozillaText = localFont({
-    src: "../public/fonts/Mozilla_Text/MozillaText-VariableFont_wght.ttf",
+    src: "../../public/fonts/Mozilla_Text/MozillaText-VariableFont_wght.ttf",
     preload: true,
     display: "swap",
     variable: "--font-my", // Optional, for CSS variables
