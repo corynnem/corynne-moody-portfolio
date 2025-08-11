4a5dd71b47494b32c514c36215c1b9a99ff5477b
Corynne Moody <corynnemoody@MacBookPro.attlocal.net>
2025-08-11 16:49:43 -0400

    rm unused

--- components/Home/styles.tsx
+++ components/Home/styles.tsx
@@ -20,41 +20,24 @@ const useStyles = makeStyles()((theme) => ({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    alignContent: 'center',
+    alignContent: "center",
   },
   title: {
     fontFamily: mozillaText.style.fontFamily,
     color: "#F1E3D3",
     fontWeight: 500,
     fontSize: "4rem",
-    [theme.breakpoints.down('sm')]: {
-        fontSize: "2.4rem",
-    }
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2.4rem",
+    },
   },
   subtitle: {
     fontFamily: mozillaText.style.fontFamily,
     color: "#F1E3D3",
     fontSize: "1.5rem",
-    [theme.breakpoints.down('sm')]: {
-        fontSize: "1rem",
-    }
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
+    },
   },
-  nav: {
-    fontFamily: mozillaText.style.fontFamily,
-    backgroundColor: "#720026",
-    borderRadius: "50px",
-    padding: theme.spacing(1, 2),
-    height: '500px'
-  },
-  navButton: {
-    color: "#F1E3D3",
-    fontFamily: mozillaText.style.fontFamily,
-    padding: theme.spacing(0, 5),
-  },
-  navSection: {
-    width: "100vw",
-    display: "flex",
-    justifyContent: 'center',
-  }
 }));
 export default useStyles;
