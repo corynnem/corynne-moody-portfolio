687c63c52dbc1cf5ee103cd42dfa9ab72f1d3b39
Corynne Moody <corynnemoody@MacBookPro.attlocal.net>
2025-08-11 21:32:39 -0400

    create mobile responsive navbar including, home, about, projects, and contact pages

--- app/styles.tsx
+++ app/styles.tsx
@@ -1,9 +1,33 @@
 import { makeStyles } from "tss-react/mui";
-
+import { theme } from "../theme";
+import { mozillaText } from "./ui/fonts";
 const useStyles = makeStyles()(() => ({
   body: {
     height: "100vh",
     width: "100vw",
+  },
+  nav: {
+    fontFamily: mozillaText.style.fontFamily,
+    backgroundColor: "#720026",
+    borderRadius: "50px",
+    padding: theme.spacing(1, 2),
+    height: "500px",
+  },
+  navButton: {
+    color: "#F1E3D3",
+    fontFamily: mozillaText.style.fontFamily,
+    padding: theme.spacing(0, 5),
+  },
+  navSection: {
+    width: "100vw",
+    display: "flex",
+    justifyContent: "center",
+    position: "absolute",
+    marginTop: '50px',
+    zIndex: 9999,
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '20px',
+    }
   },
 }));
 export default useStyles;
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Box, Button } from "@mui/material";
+
+import { useMediaQuery } from "@mui/material";
+import useStyles from "./styles";
+import { navItems, NavItems } from "./helper";
+import { theme } from "@/theme";
+
+export const Navbar = () => {
+  const [activeNavItem, setActiveNavItem] = useState(NavItems.Home);
+  const { classes } = useStyles();
+  const isBelowSm = useMediaQuery(theme.breakpoints.down('sm'));
+  console.log(isBelowSm);
+
+  return (
+    <Box className={`${classes.nav} `}>
+      {navItems.map((navItem) => {
+        const { label, icon, link, key } = navItem;
+        const isActiveNavItem = activeNavItem === key;
+        return (
+          <Button
+            className={classes.navButton}
+            disableRipple
+            href={link}
+            key={key}
+            variant={isActiveNavItem ? "contained" : "text"}
+            sx={
+              isActiveNavItem
+                ? { backgroundColor: "#4F000B" }
+                : { backgroundColor: "transparent" }
+            }
+            onClick={() => setActiveNavItem(key)}
+          >
+            {isBelowSm ? icon : label}
+          </Button>
+        );
+      })}
+    </Box>
+  );
+};
